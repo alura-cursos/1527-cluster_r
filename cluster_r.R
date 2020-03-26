bcb5add0c882e714bbbd06cbb7190d5c4c656dbd
gabriel lima <gabriel.totus@gmail.com>
2020-03-26 13:01:03 -0300

    script aula 3

--- cluster_r.R
+++ cluster_r.R
@@ -32,3 +32,68 @@ filmes_transf <- filmes_transf %>%
 ## normalizando a base de dados
 dados_normalizados <- scale(filmes_transf)
 dados_normalizados <- data.frame(dados_normalizados)
+
+
+#### 3. KMEANS CLUSTER ####
+#### 3.1 Entendendo o algoritmos Kmeans ####
+#Teoria
+
+#### 3.2 Criando Cluster com R ####
+set.seed(1987) #função pra gerar resultados iguais
+
+## criando cluster para 3 agrupamentos
+resultado_cluster <- kmeans(dados_normalizados,centers = 3)
+
+## verificando resultado da função kmeans
+resultado_cluster
+
+## vetor com os clusters para cada registro
+resultado_cluster$cluster
+
+## dataframe com os centros para cada agrupamento para cada variavel(coluna)
+resultado_cluster$centers
+
+View(resultado_cluster$centers)
+
+## vetor com as soma dos quadrados dentro do cluster. Espera-se que esse valor seja o mais baixo poss?vel para cada cluster, 
+# porque se deseja ter homogeneidade dentro dos clusters
+resultado_cluster$withinss
+
+## Soma dos quadrados entre os clusters. Resulta a M?dia de distancia entre os centros de cada cluster,
+# para se ter cluster heterogeneos, este valor deve ser o mais algo poss?vel.
+resultado_cluster$betweenss  
+
+## tamanho de cada cluster
+resultado_cluster$size
+
+#### 3.3 Plotando os clusters ####
+
+## criando visualização dos cluster com o pacote 'cluster'
+#install.packages("cluster")
+library(cluster)
+clusplot(x = dados_normalizados, resultado_cluster$cluster, color=TRUE, shade=TRUE)#, labels=3)#lines=3,
+
+
+## criando visualização dos cluster com o pacote 'fpc'
+#install.packages("fpc")
+library(fpc)
+plotcluster(x = dados_normalizados, resultado_cluster$cluster,ignorenum = T)
+
+
+## criando gráficos com os centros
+centros <- resultado_cluster$centers
+
+#install.packages('reshape2')
+library(reshape2)
+centros_2 <- melt(centros) #,id.vars = 'genero')
+
+#atribuindo novos nomes da coluna e convertendo coluna cluster para factor
+colnames(centros_2) <- c('cluster','genero','centro')
+centros_2$cluster   <- as.factor(centros_2$cluster)
+
+# criando gráficos com a lib ggplot2, pra cada cluster
+#install.packages('ggplot2')
+library(ggplot2)
+ggplot(data = centros_2 ) + 
+  geom_bar(aes(x = genero,y = centro,fill = cluster),stat = 'identity') + 
+  facet_grid(cluster ~ .)
